be0e66d00ad52fbdc1b0a6aec54d2bf47b280acf
Nanda Arya Putra <nandaarya404@gmail.com>
2023-11-23 22:40:01 +0700

    add pending intent to notification

--- app/src/main/java/com/example/kerjakuapp/GeofenceBroadcastReceiver.kt
+++ app/src/main/java/com/example/kerjakuapp/GeofenceBroadcastReceiver.kt
@@ -2,6 +2,7 @@ package com.example.kerjakuapp
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -44,12 +45,23 @@ class GeofenceBroadcastReceiver : BroadcastReceiver() {
     }
 
     private fun sendNotification(context: Context, geofenceTransitionDetails: String) {
+        val intentNotification = Intent(context, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            0,
+            intentNotification,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
         val mNotificationManager =
             context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val mBuilder = NotificationCompat.Builder(context, CHANNEL_ID)
             .setContentTitle(geofenceTransitionDetails)
             .setContentText("Anda sudah bisa absen sekarang :)")
             .setSmallIcon(R.drawable.ic_baseline_notifications_active_24)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel =
                 NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH)
