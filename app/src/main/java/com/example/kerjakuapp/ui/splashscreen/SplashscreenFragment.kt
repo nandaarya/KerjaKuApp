e4709467bc78c821a6b7191e70e3198c578274f6
Nanda Arya Putra <nandaarya404@gmail.com>
2024-05-14 21:28:26 +0700

    fix bug splashscreen stuck when paused before navigate

--- app/src/main/java/com/example/kerjakuapp/ui/splashscreen/SplashscreenFragment.kt
+++ app/src/main/java/com/example/kerjakuapp/ui/splashscreen/SplashscreenFragment.kt
@@ -2,8 +2,8 @@ package com.example.kerjakuapp.ui.splashscreen
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
@@ -11,10 +11,12 @@ import androidx.navigation.fragment.findNavController
 import com.example.kerjakuapp.R
 import com.example.kerjakuapp.databinding.FragmentSplashscreenBinding
 
+@Suppress("DEPRECATION")
 @SuppressLint("CustomSplashScreen")
 class SplashscreenFragment : Fragment() {
 
     private val splashTimeOut: Long = 2000
+    private val handler = Handler()
 
     private var _binding: FragmentSplashscreenBinding? = null
 
@@ -28,11 +30,24 @@ class SplashscreenFragment : Fragment() {
         return binding?.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onResume() {
+        super.onResume()
+        scheduleNavigation()
+    }
 
-        binding?.root?.postDelayed({
-            findNavController().navigate(R.id.action_splashscreenFragment_to_loginFragment)
-        }, splashTimeOut)
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(navigationRunnable)
+    }
+
+    private val navigationRunnable = Runnable {
+        val navController = findNavController()
+        if (navController.currentDestination?.id != R.id.loginFragment) {
+            navController.navigate(R.id.action_splashscreenFragment_to_loginFragment)
+        }
+    }
+
+    private fun scheduleNavigation() {
+        handler.postDelayed(navigationRunnable, splashTimeOut)
     }
 }
